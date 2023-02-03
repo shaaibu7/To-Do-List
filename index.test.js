35bc96500ea6b41d26c74cb4ab5fe4965a2768ee
Zeph180 <zephrichards1@gmail.com>
2023-02-03 14:04:17 +0300

    Group with describe

--- index.test.js
+++ index.test.js
@@ -3,47 +3,49 @@ import Features from './src/module/functionality.js';
 
 const newFeatures = new Features();
 
-test('testing addition of list items', () => {
-  document.body.innerHTML = `
-  <input type="text" id="task-input" />
-  <ul class="list-content" id="ul-element">
+describe('test dom manipulation', () => {
+  test('testing addition of list items', () => {
+    document.body.innerHTML = `
+    <input type="text" id="task-input" />
+    <ul class="list-content" id="ul-element">
+  
+    </ul>
+  `;
 
-  </ul>
-`;
+    const taskInput = document.getElementById('task-input');
+    const ulContainer = document.getElementById('ul-element');
 
-  const taskInput = document.getElementById('task-input');
-  const ulContainer = document.getElementById('ul-element');
+    const listItem = document.createElement('li');
+    listItem.textContent = `${taskInput.value}`;
+    ulContainer.appendChild(listItem);
+    const list = document.querySelectorAll('li');
 
-  const listItem = document.createElement('li');
-  listItem.textContent = `${taskInput.value}`;
-  ulContainer.appendChild(listItem);
-  const list = document.querySelectorAll('li');
+    function taskValue(task) {
+      taskInput.value = task;
+    }
+    // const taskInput = document.getElementById('task-input');
+    newFeatures.addTaskList(taskValue('testing'));
 
-  function taskValue(task) {
-    taskInput.value = task;
-  }
-  // const taskInput = document.getElementById('task-input');
-  newFeatures.addTaskList(taskValue('testing'));
+    expect(list).toHaveLength(1);
+  });
 
-  expect(list).toHaveLength(1);
-});
+  test('testing removal of task', () => {
+    document.body.innerHTML = `
+    <input type="text" id="task-input" value="software" />
+    <ul class="list-content" id="ul-element">
+  
+    </ul>
+  `;
 
-test('testing removal of task', () => {
-  document.body.innerHTML = `
-  <input type="text" id="task-input" value="software" />
-  <ul class="list-content" id="ul-element">
+    const taskInput = document.getElementById('task-input');
 
-  </ul>
-`;
+    // const listItem = document.createElement('li');
+    // listItem.textContent = `${taskInput.value}`;
+    // ulContainer.appendChild(listItem);
+    const list = document.querySelectorAll('li');
+    newFeatures.addTaskList(taskInput.value);
+    newFeatures.removeTask('index');
 
-  const taskInput = document.getElementById('task-input');
-
-  // const listItem = document.createElement('li');
-  // listItem.textContent = `${taskInput.value}`;
-  // ulContainer.appendChild(listItem);
-  const list = document.querySelectorAll('li');
-  newFeatures.addTaskList(taskInput.value);
-  newFeatures.removeTask('index');
-
-  expect(list).toHaveLength(0);
+    expect(list).toHaveLength(0);
+  });
 });
